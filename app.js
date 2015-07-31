c47a4289e06d05073a0a6e1cef63e1bf6b70ac70
Karl <khughes.me@gmail.com>
2015-07-31 16:35:15 -0500

    Initial work

---
+++ app.js
@@ -0,0 +1,49 @@
+var google = require('google');
+var json2csv = require('json2csv');
+var fs = require('fs');
+var async = require("async");
+
+var results = [];
+
+var fields = ['search', 'page_title', 'google_link'];
+
+var searches = require('./searches.js').getSearches();
+
+google.resultsPerPage = 3;
+
+async.each(searches,
+    function(search, callback) {
+        google(search, function (err, next, links) {
+            console.log(links);
+            if (links) {
+                var title = links[0].title;
+                if (title.indexOf("Map for ") > -1) {
+                    title = links[1].title;
+                    var link = links[1].link;
+                } else {
+                    title = links[0].title;
+                    var link = links[0].link;
+                }
+                var row = {
+                    'search': search,
+                    'title': title,
+                    'link': link
+                };
+                results.push(row);
+            }
+            callback();
+        });
+    },
+    function(err){
+        // All tasks are done now
+        if (results[0]) {
+            json2csv({ data: results }, function(err, csv) {
+                if (err) console.log(err);
+                fs.writeFile('test.csv', csv, function(err) {
+                    if (err) throw err;
+                    console.log('file saved');
+                });
+            });
+        }
+    }
+);
